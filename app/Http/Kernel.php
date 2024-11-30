dbcb456358cfc6e48758aec92fe89bdeb37dc9ec
Rasha yaseen <yaseenrasha4@gmail.com>
2024-11-30 11:02:42 +0300

    Migrations/models & relation/controllers/login & register/middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'manager' => \App\Http\Middleware\manager::class,
+        'employee' => \App\Http\Middleware\employee::class,
     ];
 }
