b2c7184927b1ccd598a93fba716f0e48d4d93515
talaalosili <talaalosili@gmail.com>
2024-12-20 18:26:58 +0300

    dashbord desgin

--- resources/views/include/dashboard/sidebar.blade.php
+++ resources/views/include/dashboard/sidebar.blade.php
@@ -13,7 +13,7 @@
 
       @if(Auth::user()->role == 'manager')
       <li class="nav-item">
-        <a class="nav-link " href="{{route('users.index')}}">
+        <a class="nav-link {{ request()->routeIs('users.index') ? 'active' : '' }}" href="{{route('users.index')}}">
           <i class="bi bi-person"></i>
           <span>Users</span>
         </a>
@@ -22,7 +22,7 @@
 
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('categories.index')}}">
+        <a class="nav-link {{ request()->routeIs('categories.index') ? 'active' : '' }} collapsed " href="{{route('categories.index')}}">
           <i class="bi bi-menu-button-wide"></i><span>Categories</span></i>
         </a>
       
@@ -31,84 +31,57 @@
 
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('subCategories.index')}}">
+        <a class="nav-link {{ request()->routeIs('subCategories.index') ? 'active' : '' }} collapsed" href="{{route('subCategories.index')}}">
           <i class="bi bi-journal-text"></i><span>Sub Categories</span></i>
         </a>
     
       </li><!-- End Forms Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('products.index')}}">
+        <a class="nav-link {{ request()->routeIs('products.index') ? 'active' : '' }} collapsed" href="{{route('products.index')}}">
           <i class="bi bi-layout-text-window-reverse"></i><span>Products</span></i>
         </a>
      
       </li><!-- End Tables Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#">
+        <a class="nav-link {{ request()->routeIs('#') ? 'active' : '' }} collapsed" href="#">
           <i class="bi bi-bar-chart"></i><span>Orders</span></i>
         </a>
   
       </li><!-- End Charts Nav -->
 
+      
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#">
-          <i class="bi bi-gem"></i><span>Contact us</span></i>
+        <a class="nav-link {{ request()->routeIs('contacts.index') ? 'active' : '' }} collapsed" href="/contacts">
+          <i class="bi bi-envelope"></i>
+          <span>Contact Us</span>
         </a>
+      </li><!-- End Contact Page Nav -->
+
   
       </li><!-- End Icons Nav -->
 
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="">
+        <a class="nav-link {{ request()->routeIs('.index') ? 'active' : '' }} collapsed" href="">
           <i class="bi bi-person"></i>
           <span>Profile</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-faq.html">
-          <i class="bi bi-question-circle"></i>
-          <span>F.A.Q</span>
-        </a>
-      </li><!-- End F.A.Q Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="/contacts">
-          <i class="bi bi-envelope"></i>
-          <span>Contact</span>
-        </a>
-      </li><!-- End Contact Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li><!-- End Register Page Nav -->
-
+  
+     
       <li class="nav-item">
         <a class="nav-link collapsed" href="pages-login.html">
           <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
+          <span>Logout</span>
         </a>
       </li><!-- End Login Page Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-error-404.html">
-          <i class="bi bi-dash-circle"></i>
-          <span>Error 404</span>
-        </a>
-      </li><!-- End Error 404 Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
-          <i class="bi bi-file-earmark"></i>
-          <span>Blank</span>
-        </a>
-      </li><!-- End Blank Page Nav -->
-
+    
+      
     </ul>
 
   </aside><!-- End Sidebar-->
